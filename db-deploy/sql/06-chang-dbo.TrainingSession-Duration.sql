53c3ba3a2d463f3dadd5b3ff1b67b128c0343f5d
RAN <wangrandk@users.noreply.github.com>
2024-07-01 18:19:56 +0200

    Delete 06-chang-dbo.TrainingSession-Duration.sql

--- db-deploy/sql/06-chang-dbo.TrainingSession-Duration.sql
+++
@@ -1,7 +0,0 @@
-sp_rename 'dbo.TrainingSession.[Calories]', 'Calory', 'COLUMN';   
-GO
-sp_rename 'dbo.TrainingSession.[AdjustedDistance]', 'TotalDistance', 'COLUMN';   
-GO
-
-alter TABLE dbo.TrainingSession
-    alter column Duration decimal(7, 2);
