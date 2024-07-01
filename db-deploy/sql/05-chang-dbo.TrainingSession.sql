c981d60e8e4bac2173d58f93d33a4b8e935634b6
RAN <wangrandk@users.noreply.github.com>
2024-07-01 17:50:53 +0200

    update

--- db-deploy/sql/05-chang-dbo.TrainingSession.sql
+++ db-deploy/sql/05-chang-dbo.TrainingSession.sql
@@ -5,7 +5,3 @@ alter TABLE dbo.TrainingSession
     alter column AdjustedDistance decimal(9, 2)
 GO
 
-sp_rename 'dbo.TrainingSession.[Calories]', 'Calory', 'COLUMN';   
-GO
-sp_rename 'dbo.TrainingSession.[AdjustedDistance]', 'TotalDistance', 'COLUMN';   
-GO
--- db-deploy/sql/06-chang-dbo.TrainingSession-calories.sql
+++ db-deploy/sql/06-chang-dbo.TrainingSession-calories.sql
@@ -2,4 +2,8 @@
 -- sp_rename 'table_name.[old_column_name]', 'new_column_name', 'COLUMN';
 
 -- Example: Rename the 'AdjustedDistance' column to 'TotalDistance'
-sp_rename 'dbo.TrainingSession.[Calory]', 'Calories', 'COLUMN';   
+-- sp_rename 'dbo.TrainingSession.[Calory]', 'Calories', 'COLUMN';   
+sp_rename 'dbo.TrainingSession.[Calories]', 'Calory', 'COLUMN';   
+GO
+sp_rename 'dbo.TrainingSession.[AdjustedDistance]', 'TotalDistance', 'COLUMN';   
+GO
