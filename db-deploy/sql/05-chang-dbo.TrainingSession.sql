21defc5c1dab8fbb77839ca2c9ddd8556e85c680
RAN <wangrandk@users.noreply.github.com>
2024-07-01 17:58:10 +0200

    update

--- db-deploy/sql/05-chang-dbo.TrainingSession.sql
+++ db-deploy/sql/05-chang-dbo.TrainingSession.sql
@@ -2,6 +2,18 @@ alter TABLE dbo.TrainingSession
     alter column Calories decimal(7, 2);
 
 alter TABLE dbo.TrainingSession
+    alter column Duration decimal(7, 2);
+
+alter TABLE dbo.TrainingSession
     alter column AdjustedDistance decimal(9, 2)
 GO
 
+-- Syntax to rename a column
+-- sp_rename 'table_name.[old_column_name]', 'new_column_name', 'COLUMN';
+
+-- Example: Rename the 'AdjustedDistance' column to 'TotalDistance'
+-- sp_rename 'dbo.TrainingSession.[Calory]', 'Calories', 'COLUMN';   
+sp_rename 'dbo.TrainingSession.[Calories]', 'Calory', 'COLUMN';   
+GO
+sp_rename 'dbo.TrainingSession.[AdjustedDistance]', 'TotalDistance', 'COLUMN';   
+GO
